d0692bfc7bc407a041392308ed4e08e26bc0afd8
Claus Hunsen <claus.hunsen@msg.group>
2020-04-06 14:01:32 +0200

    Use multi-line strings for custom OPA5 matchers
    For better readability, the custom OPA5 matchers 'ItemBindingMatcher'
    and 'ParentMatcher' are now generated using multi-line strings and
    appropriate regexes (to remove leading whitespace characters etc.).
    
    Signed-off-by: Claus Hunsen <claus.hunsen@msg.group>

--- scripts/popup/model/code-generation/opa5/ItemBindingMatcherBuilder.js
+++ scripts/popup/model/code-generation/opa5/ItemBindingMatcherBuilder.js
@@ -19,50 +19,62 @@ sap.ui.define([
          * @returns {string}
          */
         generate: function () {
-            var aLines = [];
-            aLines.push('sap.ui.define([\n');
-            aLines.push(Array(2).join('\t') + '"sap/ui/test/matchers/Matcher"\n');
-            aLines.push('], function (Matcher) {\n');
-            aLines.push(Array(2).join('\t') + '"use strict";\n\n');
-            aLines.push(Array(2).join('\t') + 'return Matcher.extend("' + this.__namespace + '\.<testPath>\.' + 'customMatcher.ItemBindingMatcher", {\n');
-            aLines.push(Array(3).join('\t') + 'metadata: {\n');
-            aLines.push(Array(4).join('\t') + 'publicMethods: ["isMatching"],\n');
-            aLines.push(Array(4).join('\t') + 'properties: {\n');
-            aLines.push(Array(5).join('\t') + 'modelName: {type: "string"},\n');
-            aLines.push(Array(5).join('\t') + 'propertyName: {type: "string"},\n');
-            aLines.push(Array(5).join('\t') + 'propertyValue: {type: "string"}\n');
-            aLines.push(Array(4).join('\t') + '}\n');
-            aLines.push(Array(3).join('\t') + '},\n\n');
-            aLines.push(Array(3).join('\t') + 'isMatching: function (oControl) {\n');
-            aLines.push(Array(4).join('\t') + 'if (!this.getPropertyName()) {\n');
-            aLines.push(Array(5).join('\t') + 'this._oLogger.error("No propertyName given for Control \'" + oControl + "\'");\n');
-            aLines.push(Array(5).join('\t') + 'return false;\n');
-            aLines.push(Array(4).join('\t') + '}\n');
-            aLines.push(Array(4).join('\t') + 'var oBindingContext = oControl.getBindingContext(this.getModelName());\n');
-            aLines.push(Array(4).join('\t') + 'if (!oBindingContext) {\n');
-            aLines.push(Array(5).join('\t') + 'this._oLogger.error("No bindingContext found for \'" + oControl + "\' and modelName " + this.getModelName());\n');
-            aLines.push(Array(5).join('\t') + 'return false;\n');
-            aLines.push(Array(4).join('\t') + '}\n');
-            aLines.push(Array(4).join('\t') + 'var sPropertyValue = oBindingContext.getObject()[this.getPropertyName()];\n');
-            aLines.push(Array(4).join('\t') + 'if (!sPropertyValue) {\n');
-            aLines.push(Array(5).join('\t') + 'this._oLogger.error("No value found for \'" + oControl + "\',modelName " + this.getModelName() + " and property " + this.getPropertyName());\n');
-            aLines.push(Array(5).join('\t') + 'return false;\n');
-            aLines.push(Array(4).join('\t') + '}\n');
+            var sDefinition = '\
+                sap.ui.define([ \n\
+                    "sap/ui/test/matchers/Matcher" \n\
+                ], function (Matcher) { \n\
+                    "use strict"; \n\
+                    \n\
+                    return Matcher.extend("' + this.__namespace + '.<testPath>.customMatcher.ItemBindingMatcher", { \n\
+                        metadata: { \n\
+                            publicMethods: ["isMatching"], \n\
+                            properties: { \n\
+                                modelName: {type: "string"}, \n\
+                                propertyName: {type: "string"}, \n\
+                                propertyValue: {type: "string"} \n\
+                            } \n\
+                        }, \n\
+                        \n\
+                        isMatching: function (oControl) { \n\
+                            if (!this.getPropertyName()) { \n\
+                                this._oLogger.error("No propertyName given for control \'" + oControl + "\'"); \n\
+                                return false; \n\
+                            } \n\
+                            \n\
+                            var oBindingContext = oControl.getBindingContext(this.getModelName()); \n\
+                            if (!oBindingContext) { \n\
+                                this._oLogger.error("No bindingContext found for \'" + oControl + "\' and modelName " + this.getModelName()); \n\
+                                return false; \n\
+                            } \n\
+                            \n\
+                            var sPropertyValue = oBindingContext.getObject()[this.getPropertyName()]; \n\
+                            if (!sPropertyValue) { \n\
+                                this._oLogger.error("No value found for \'" + oControl + "\', modelName " + this.getModelName() + ", and property " + this.getPropertyName()); \n\
+                                return false; \n\
+                            } \n\
+                            \n\
+                            if (this.getPropertyValue()) { \n\
+                                if (sPropertyValue === this.getPropertyValue()) { \n\
+                                    return true; \n\
+                                } else { \n\
+                                    return false; \n\
+                                } \n\
+                            } else { \n\
+                                return true; \n\
+                            } \n\
+                        } \n\
+                    }); \n\
+                });';
 
-            aLines.push(Array(4).join('\t') + 'if (this.getPropertyValue()) {\n');
-            aLines.push(Array(5).join('\t') + 'if (sPropertyValue === this.getPropertyValue()) {\n');
-            aLines.push(Array(6).join('\t') + 'return true;\n');
-            aLines.push(Array(5).join('\t') + '} else {\n');
-            aLines.push(Array(6).join('\t') + 'return false;\n');
-            aLines.push(Array(5).join('\t') + '}\n');
-            aLines.push(Array(4).join('\t') + '} else {\n');
+            // remove leading whitespace for code editor
+            var iLengthWhitespace = sDefinition.match(/^([ \t]+)/g)[0].length;
+            var oLeadingWhitespaceRegex = new RegExp("^[ \\t]{" + iLengthWhitespace + "}", "gm");
+            sDefinition = sDefinition.replace(oLeadingWhitespaceRegex, "");
 
-            aLines.push(Array(5).join('\t') + 'return true;\n');
-            aLines.push(Array(4).join('\t') + '}\n');
-            aLines.push(Array(3).join('\t') + '}\n');
-            aLines.push(Array(2).join('\t') + '});\n');
-            aLines.push('});');
-            return aLines.reduce((a, b) => a + b, '');
+            // remove trailing whitespace for code editor
+            sDefinition = sDefinition.replace(/([ ]+)$/gm, "");
+
+            return sDefinition;
         }
     });
 
