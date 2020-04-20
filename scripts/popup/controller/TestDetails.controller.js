ui5-community/ui5-journey-recorder

commit fe1163e90ca75c36586f6e1891ca84e5e97a53b2

Replace dialog 'RecordDialog' with BusyDialog control
As Busydialog controls do not close on navigation, close the dialog
manually then.

Signed-off-by: Claus Hunsen <claus.hunsen@msg.group>

scripts/popup/controller/TestDetails.controller.js
@@ -442,6 +442,7 @@ sap.ui.define([
 
             RecordController.getInstance().setCurrentElement(oData);
             RecordController.getInstance().focusPopup();
+            this._oRecordDialog.close();
 
             var sRouterTarget = this._bQuickMode ? "elementCreateQuick" : "elementCreate";
             this.getRouter().navTo(sRouterTarget, {

scripts/popup/fragment/RecordDialog.fragment.xml
@@ -1,38 +1,11 @@
 <core:FragmentDefinition
     xmlns="sap.m"
-    xmlns:l="sap.ui.layout"
-    xmlns:f="sap.ui.layout.form"
-    xmlns:code="sap.ui.codeeditor"
-    xmlns:u="sap.ui.unified"
     xmlns:core="sap.ui.core">
-    <Dialog
-        contentWidth="256px"
-        contentHeight="256px"
-        verticalScrolling="false"
-        showHeader="true"
-        draggable="true"
-        title="Recording Information">
-        <content>
-            <VBox fitContainer="true" height="100%">
-                <HBox fitContainer="true" height="100%">
-                    <core:Icon
-                        size="128px"
-                        decorative="false"
-                        press="onStopRecord"
-                        color="black"
-                        src="sap-icon://stop">
-                        <core:layoutData>
-                            <FlexItemData alignSelf="Center"/>
-                        </core:layoutData>
-                    </core:Icon>
-                    <layoutData>
-                        <FlexItemData alignSelf="Center"/>
-                    </layoutData>
-                </HBox>
-                <layoutData>
-                    <FlexItemData alignSelf="Center"/>
-                </layoutData>
-            </VBox>
-        </content>
-    </Dialog>
+
+    <BusyDialog
+        title="Select test element"
+        text="Waiting until user selects test element in tested tab..."
+        showCancelButton="true"
+        cancelButtonText="Stop recording" />
+
 </core:FragmentDefinition>

commit 0493abebc4a662484ef445176c019590de984215

Stop recording when closing record dialog
During recording, the view 'TestDetails' displays the record dialog,
which is a BusyDialog control and indicates on-going recording. When
clicking the inherit button "Stop recording", the recording is to be
stopped and the page injection should not lock interaction with the page
anymore.

The release of the page locking has been broken by a regression
introduced by commit fe1163e90ca75c36586f6e1891ca84e5e97a53b2. This is
fixed by attaching the function 'onStopRecord' to the record dialog's
closing event again: The page is unlocked if and only if the user
pressed the button 'Stop recording' actively.

This fixes #43.

scripts/popup/controller/TestDetails.controller.js
@@ -189,10 +189,14 @@ sap.ui.define([
 
         /**
          *
+         * @param {sap.ui.base.Event} oControlEvent the event attached to the closing action
          */
-        onStopRecord: function () {
-            RecordController.getInstance().stopRecording();
-            this._oRecordDialog.close();
+        onStopRecord: function (oControlEvent) {
+            // only stop recording iff the user pressed the cancel button 'Stop recording'
+            if (oControlEvent.getParameter("cancelPressed")) {
+                RecordController.getInstance().stopRecording();
+                this._oRecordDialog.close(); // dialog may be already closed
+            }
         },
 
         /**
@@ -553,6 +557,7 @@ sap.ui.define([
                 controller: this
             }).then(function(oRecordDialog) {
                 this._oRecordDialog = oRecordDialog;
+                this._oRecordDialog.attachClose(this.onStopRecord, this);
             }.bind(this));
 
             this._oMessagePopover = new MessagePopover({