ui5-community/ui5-journey-recorder

commit a8e31a5fca6941eb3563873286d42bc6a0bef5c2

Fixed some small stuff to faster page reaktions
Signed-off-by: Adrian Marten <adrian.marten@msg.group>

scripts/popup/controller/TestStep.controller.js
@@ -153,7 +153,10 @@ sap.ui.define([
             this.getModel('viewModel').setProperty('/blocked', true);
             this.getModel('viewModel').setProperty('/isInjected', RecordController.getInstance().isInjected());
             this._setValidAttributeTypes();
-            this._updatePreview();
+            Promise.all([
+                this._updatePreview(),
+                this._validateSelectedItemNumber()
+            ]);
 
             if (this.getModel('viewModel').setProperty('/isInjected')) {
                 this.getModel('viewModel').setProperty('/blocked', false);
@@ -254,6 +257,7 @@ sap.ui.define([
             this._updateSubActionTypes();
             this._adjustDomChildWith(this._oModel.getProperty("/element/item"));
             this._updatePreview();
+            //TODD @Adrian: reloading all found items should not be necessary
         },
 
         /**
@@ -333,7 +337,10 @@ sap.ui.define([
         onAddAttribute: function (oEvent) {
             this._add("/element/attributeFilter");
             this.byId("atrElementsPnl").setExpanded(true);
-            this._updatePreview();
+            Promise.all([
+                this._updatePreview(),
+                this._validateSelectedItemNumber()
+            ]);
         },
 
         /**
@@ -358,7 +365,10 @@ sap.ui.define([
         onFindAttribute: function (oEvent) {
             var oItem = this._oModel.getProperty("/element/item");
             this._findBestAttributeDefaultSetting(oItem, true).then(function () {
-                this._updatePreview();
+                Promise.all([
+                    this._updatePreview(),
+                    this._validateSelectedItemNumber()
+                ]);
             }.bind(this));
         },
 
@@ -373,7 +383,10 @@ sap.ui.define([
                 subCriteriaType: ""
             });
 
-            this._updatePreview();
+            Promise.all([
+                this._updatePreview(),
+                this._validateSelectedItemNumber()
+            ]);
         },
 
         /**
@@ -382,7 +395,10 @@ sap.ui.define([
         onAttributeTypeChanged: function (oEvent) {
             var oCtx = oEvent.getSource().getBindingContext("viewModel");
             this._updateAttributeTypes(oCtx);
-            this._updatePreview();
+            Promise.all([
+                this._updatePreview(),
+                this._validateSelectedItemNumber()
+            ]);
         },
 
         /**
@@ -391,7 +407,10 @@ sap.ui.define([
         onCriteriaTypeChanged: function (oEvent) {
             var oCtx = oEvent.getSource().getBindingContext("viewModel");
             this._updateCriteriaType(oCtx);
-            this._updatePreview();
+            Promise.all([
+                this._updatePreview(),
+                this._validateSelectedItemNumber()
+            ]);
         },
 
         /**
@@ -400,7 +419,10 @@ sap.ui.define([
         onSubCriteriaTypeChanged: function (oEvent) {
             var oCtx = oEvent.getSource().getBindingContext("viewModel");
             this._updateSubCriteriaType(oCtx);
-            this._updatePreview();
+            Promise.all([
+                this._updatePreview(),
+                this._validateSelectedItemNumber()
+            ]);
         },
 
         /**
@@ -433,13 +455,18 @@ sap.ui.define([
                 //we anyways only have length for the moment - change it to integer..
                 this._oModel.setProperty(oAttributeCtx.getPath() + "/criteriaValue", parseInt(oAttribute.criteriaValue, 10));
             }
-            this.onUpdatePreview();
+            Promise.all([
+                this._updatePreview(),
+                this._validateSelectedItemNumber()
+            ]);
         },
 
         //#region Element initialization and updating
 
         /**
          *
+         * @param {object} oItem the item to use for test recording
+         * @param {boolean} bAssertion marker if the Action is an assertion
          */
         initializeTestElement: function (oItem, bAssertion) {
             this._oModel.setProperty("/element", JSON.parse(JSON.stringify(this._oModel.getProperty("/elementDefault"))));
@@ -448,6 +475,7 @@ sap.ui.define([
             }
             this.byId("atrElementsPnl").setExpanded(false);
             this._bShowCodeOnly = false;
+            debugger;
             this._setItem(oItem);
 
             //in case we are in "TYP" after opening, set focus to input field..
@@ -560,7 +588,7 @@ sap.ui.define([
                 }
 
                 if (typeof oMerged.preferredType !== "undefined" && oMerged.preferredType !== null) {
-                    this._oModel.setProperty("/element/property/type", oMerged.preferredType)
+                    this._oModel.setProperty("/element/property/type", oMerged.preferredType);
                 }
 
                 if (typeof oMerged.defaultEnter !== "undefined" && oMerged.defaultEnter !== null) {
@@ -601,11 +629,11 @@ sap.ui.define([
          *
          */
         _findBestAttributeDefaultSetting: function (oItem, bForcePopup) {
-            return new Promise(function (resolve, reject) {
-                this._adjustAttributeDefaultSetting(oItem).then(this._getFoundElements.bind(this)).then(function (aReturn) {
+            return new Promise(function (resolve) {
+                this._adjustAttributeDefaultSetting(oItem).then(function (aReturn) {
                     //in case we still have >1 item - change to
                     if (this._oModel.getProperty("/element/property/selectItemBy") === "ATTR" &&
-                        ((aReturn.length > 1 && this._oModel.getProperty("/element/property/type") === 'ACT') || bForcePopup === true)) {
+                        ((this._oModel.getProperty("/element/identifiedElements").length > 1 && this._oModel.getProperty("/element/property/type") === 'ACT') || bForcePopup === true)) {
                         //ok - we are still not ready - let's check if we are any kind of item, which is requiring the user to ask for binding context information..
                         //work on our binding context information..
                         var oItem = this._oModel.getProperty("/element/item");
@@ -756,9 +784,9 @@ sap.ui.define([
                 var sProp = this._oModel.getProperty("/element/property/selectItemBy");
                 if (sProp != "ATTR") {
                     this._oModel.setProperty("/element/attributeFilter", []);
-                    resolve();
+                    resolve(this._getFoundElements());
                 } else {
-                    this._findAttribute(oItem).then(resolve, reject); //generate standard, or "best fitting" (whatever that is :-)
+                    resolve(this._findAttribute(oItem)); //generate standard, or "best fitting" (whatever that is :-)
                 }
             }.bind(this));
         },
@@ -855,8 +883,8 @@ sap.ui.define([
                 this._getFoundElements().then(function (aReturn) {
 
                     if (aReturn.length === 1) { // early exit if possible: the less attributes the better
-                        resolve();
-                        return;
+                        resolve(aReturn);
+                        return aReturn;
                     }
 
                     // 6) now add the label text if possible and static
@@ -870,8 +898,7 @@ sap.ui.define([
                     }
 
                     // 7) finally, resolve
-                    resolve();
-
+                    resolve(aReturn);
                 }.bind(this));
             }.bind(this));
         },
@@ -1013,8 +1040,8 @@ sap.ui.define([
         _updatePreview: function () {
             var oItem = this._oModel.getProperty("/element");
             this._suspendBindings();
-            this._adjustBeforeSaving(oItem).then(function (oElementFinal) {
-                //can not understand why we need this
+            return this._adjustBeforeSaving(oItem).then(function (oElementFinal) {
+                //can not understand why we need this, here.
                 /* this._getFoundElements().then(function (aElements) {
 
                     if (aElements.length !== 1) {
@@ -1037,6 +1064,23 @@ sap.ui.define([
                 codeSettings.language = this.getModel('settings').getProperty('/settings/defaultLanguage');
                 codeSettings.execComponent = this.getOwnerComponent();
                 this.getModel("viewModel").setProperty("/code", CodeHelper.getItemCode(codeSettings, oElementFinal, this.getOwnerComponent()).join("\n").trim());
+            }.bind(this));
+        },
+
+        /**
+         * 
+         */
+        _validateSelectedItemNumber: function () {
+            return this._getFoundElements().then(function (aElements) {
+                if (aElements.length !== 1) {
+                    // expand elements panel when in ACTION mode: the user has to do sth. as only one element can be selected for an action
+                    if (this._oModel.getProperty("/element/property/type") === 'ACT') {
+                        this.byId("atrElementsPnl").setExpanded(true);
+                    }
+                }
+
+                this._checkElementNumber();
+                return aElements;
             }.bind(this));
         },
 
@@ -1540,9 +1584,12 @@ sap.ui.define([
                 component: this._oModel.getProperty("/element/item/metadata/componentName"),
                 rules: this._oModel.getProperty("/element/property/supportAssistant")
             }).then(function (oStoreResult) {
+                Promise.all([
+                    this._updatePreview(),
+                    this._validateSelectedItemNumber()
+                ]);
                 this._oModel.setProperty("/statics/supportRules", oStoreResult.rules);
                 this._oModel.setProperty("/element/supportAssistantResult", oStoreResult);
-                this._updatePreview();
                 this.byId("pnlSupAssistantRule").setBusy(false);
             }.bind(this));
 
@@ -1696,6 +1743,10 @@ sap.ui.define([
                     press: function () {
                         var aItems = this._oTableContext.getSelectedItems();
                         if (aItems && aItems.length) {
+                            Promise.all([
+                                this._updatePreview(),
+                                this._validateSelectedItemNumber()
+                            ]);
                             for (var j = 0; j < aItems.length; j++) {
                                 var oBndgCtxObj = aItems[j].getBindingContext("viewModel").getObject();
                                 this._add("/element/attributeFilter", {
@@ -1704,7 +1755,6 @@ sap.ui.define([
                                     subCriteriaType: oBndgCtxObj.bdgPath
                                 });
                             }
-                            this._updatePreview();
                         }
                         this._oSelectDialog.close();
                     }.bind(this)

scripts/popup/model/Connection.js
@@ -16,7 +16,9 @@ sap.ui.define([
             // get window info for popup window
             this._iWindowId = null;
             this._bStartImmediately = false;
-            chrome.runtime.sendMessage({type: "handshake-get-window-id"}, function (response) {
+            chrome.runtime.sendMessage({
+                type: "handshake-get-window-id"
+            }, function (response) {
                 if (response && response.type === "handshake-send-window-id") {
                     this._iWindowId = response.windowId;
                     this._bStartImmediately = response.startImmediately;
@@ -56,10 +58,10 @@ sap.ui.define([
                         // state that an injected is attempted so that no further ones are performed
                         bInjectionAttempted = true;
                         // perform the injection after some seconds
-                        setTimeout(function() {
+                        setTimeout(function () {
                             chrome.tabs.executeScript(iTabId, {
                                 file: '/scripts/content/contentInject.js'
-                            }, function() {
+                            }, function () {
                                 // remove the listener after the attempt so that it is not triggered for any further
                                 // tab updates (later, somewhere else, ...)
                                 chrome.tabs.onUpdated.removeListener(fnAttemptInjectionAfterReload);
@@ -102,7 +104,9 @@ sap.ui.define([
 
                     // 1) reload page before attempting the injection to reset any internal counters of the UI5 page (e.g., view numbers).
                     //    this triggers 'fnAttemptInjectionAfterReload'
-                    chrome.tabs.reload(this._sTabId, {bypassCache: false});
+                    chrome.tabs.reload(this._sTabId, {
+                        bypassCache: false
+                    });
                 } else {
                     reject({
                         message: "There is already a connection, please stop before opening a new one."
@@ -129,7 +133,7 @@ sap.ui.define([
          *
          * @returns {boolean} true if the connection is established, false otherwise
          */
-        isConnected: function() {
+        isConnected: function () {
             return !!this._port;
         },
 
@@ -177,7 +181,9 @@ sap.ui.define([
          * @param {object} oInformation the message to send
          */
         _sendMessage: function (oInformation) {
-            this._port.postMessage(oInformation);
+            if (this._port) {
+                this._port.postMessage(oInformation);
+            }
         },
 
         /**

commit dac9b1b54e207253f7d53945aaa488524d98f469

Fix on-the-fly update of assert execution and action checking
When handling a test item in the controller and view 'TestStep', the
state of the currently configured test step is not properly evaluated:
The on-the-fly visualization of an assert or action result (i.e., the
coloring of panel titles and the star rating) is not updated under all
circumstances. This issue is a regression caused by the changes in
commit a8e31a5fca6941eb3563873286d42bc6a0bef5c2.

This is fixed by calling the function '_validateSelectedItemNumber()' in
more places where the preview is updated.

This is related to issue #45.

Signed-off-by: Claus Hunsen <claus.hunsen@msg.group>

scripts/popup/controller/TestStep.controller.js
@@ -306,7 +306,10 @@ sap.ui.define([
          *
          */
         onUpdatePreview: function () {
-            this._updatePreview();
+            Promise.all([
+                this._updatePreview(),
+                this._validateSelectedItemNumber()
+            ]);
         },
 
         /**
@@ -326,7 +329,10 @@ sap.ui.define([
                 this._oModel.setProperty("/element/attributeFilter", aFilter);
 
                 //update preview
-                this._updatePreview();
+                Promise.all([
+                    this._updatePreview(),
+                    this._validateSelectedItemNumber()
+                ]);
             }.bind(this));
         },
 
@@ -509,6 +515,7 @@ sap.ui.define([
                 this._updateValueState(oItem);
                 this._updateSubActionTypes();
                 this._updatePreview();
+                this._validateSelectedItemNumber();
 
                 this._resumeBindings();
             }.bind(this));
@@ -937,6 +944,7 @@ sap.ui.define([
             }
 
             this._updatePreview();
+            this._validateSelectedItemNumber();
             this._check();
         },
 