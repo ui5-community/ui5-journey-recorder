c9a7e7da747b10b1820e6aa743c23b5186717407
Adrian Marten <Adrian.Marten@msg.group>
2020-04-29 09:57:24 +0200

    added 'onContinueRecording' if pressed yes, for continueRecording - Dialog, fixes #56
    Signed-off-by: Adrian Marten <Adrian.Marten@msg.group>

--- scripts/popup/controller/App.controller.js
+++ scripts/popup/controller/App.controller.js
@@ -7,23 +7,23 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("com.ui5.testing.controller.App", {
-        onInit: function() {
+        onInit: function () {
             //If necessary initialize the global model for all test records
-            if(!this.getOwnerComponent().getModel('records')) {
+            if (!this.getOwnerComponent().getModel('records')) {
                 this.getOwnerComponent().setModel(new JSONModel({}), 'records');
             }
 
             //setup the global model for constants
-            if(!this.getOwnerComponent().getModel('constants')) {
+            if (!this.getOwnerComponent().getModel('constants')) {
                 this.getOwnerComponent().setModel(new JSONModel(Utils.statics), 'constants');
             }
 
             //setup the global settings model
-            if(!this.getOwnerComponent().getModel('settings')) {
+            if (!this.getOwnerComponent().getModel('settings')) {
                 this.getOwnerComponent().setModel(GlobalSettings.getModel(), 'settings');
             }
 
-            jQuery.sap.log.setLevel(jQuery.sap.log.Level.INFO);
+            //jQuery.sap.log.setLevel(jQuery.sap.log.Level.INFO);
         }
     });
 });
