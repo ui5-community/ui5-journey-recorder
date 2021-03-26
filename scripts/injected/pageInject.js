c474b839338074c08d3371a5ecbffe0b01d33c1d
STARTIM <timo.stark@msg.group>
2021-03-26 12:16:56 +0100

    fix crash during selection

--- scripts/injected/pageInject.js
+++ scripts/injected/pageInject.js
@@ -2316,7 +2316,11 @@ var _wnd = window;
 
                                         if (aCol && aCol.length && aCol.length > x) {
                                             oReturn.tableColId = this.getUi5Id(aCol[x]);
-                                            oReturn.tableColDescr = aCol[x].getLabel ? aCol[x].getLabel().getText() : "";
+                                            if ( aCol[x].getLabel && aCol[x].getLabel() && aCol[x].getLabel.getText ) {
+                                                oReturn.tableColDescr = aCol[x].getLabel.getText();
+                                            } else {
+                                                oReturn.tableColDescr = "";
+                                            }
                                         }
                                         break;
                                     }
