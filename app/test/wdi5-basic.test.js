c07eec4c0a408c0b2cc3f910785f36d555bc0c46
Volker Buzek <volker.buzek@js-soft.com>
2022-10-10 22:39:28 +0200

    test: basic skeletion

---
+++ app/test/wdi5-basic.test.js
@@ -0,0 +1,16 @@
+const { describe, it } = require("node:test");
+const assert = require("node:assert").strict;
+const Wdi5CodeStrategy = require("../dist-codeservice/src/app/scenario/codeService/strategies/wdi5/Wdi5CodeStrategy.js").default;
+const {
+  TestScenario,
+} = require("../dist-codeservice/src/app/classes/testScenario.js");
+
+
+const _scenario = require("./Journey_220922_22_22.json");
+describe("wdi5 scenario generation", () => {
+  it("scenario", () => {
+    const scenario = TestScenario.fromJSON(JSON.stringify(_scenario));
+    const result = new Wdi5CodeStrategy().generateTestCode(scenario);
+    assert.ok(false)
+  });
+});
