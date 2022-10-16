c784ab666e3b56c2dbdc0b5630787513f217f88d
Volker Buzek <volker.buzek@js-soft.com>
2022-10-16 20:05:07 +0200

    refactor: member viz

--- app/src/app/scenario/codeService/strategies/wdi5/Wdi5IPageBuilder.ts
+++ app/src/app/scenario/codeService/strategies/wdi5/Wdi5IPageBuilder.ts
@@ -1,7 +1,7 @@
 import { Page } from 'src/app/classes/testScenario';
 
 export default abstract class Wdi5IPageBuilder {
-  private _page: Page;
+  protected _page: Page;
   constructor(page: Page) {
     this._page = page;
   }
