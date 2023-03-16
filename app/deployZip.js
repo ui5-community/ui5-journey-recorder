adc88016586de03b4768c9f35b140280ed63db3a
Adrian Marten <adrian_marten@hotmail.com>
2023-03-16 11:07:21 +0100

    Adapted the README to include the new way to install the UI5 Journey Recorder

--- app/deployZip.js
+++ app/deployZip.js
@@ -1,8 +1,14 @@
 const { zip } = require('zip-a-folder');
 const { version } = require('./package.json');
+const { argv } = require('process');
+
+let suffix = '';
+if (argv.includes('--pre')) {
+  suffix = 'nightly_'
+}
 
 const zipping = async () => {
-  zip('dist', `../journey_recorder_${version.replace(/\./gm, '-')}.zip`);
+  zip('dist', `../journey_recorder_${suffix}${version.replace(/\./gm, '-')}.zip`);
 }
 
 zipping();
