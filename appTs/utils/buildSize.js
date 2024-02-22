878f69d8ec12c586539f9ffb863bf729b37a03df
Adrian Marten <adrian_marten@hotmail.com>
2024-02-22 16:49:17 +0100

    Added build size information script at postbuild

---
+++ appTs/utils/buildSize.js
@@ -0,0 +1,31 @@
+const glob = require("glob");
+const fs = require('fs');
+const byteSize = (size) => {
+    const size_map = {
+        0: 'B',
+        1: 'kB',
+        2: 'MB',
+        3: 'GB'
+    };
+    let divided = 0;
+    while (size > 1024) {
+        divided++
+        size = size / 1024.0;
+    }
+    return `${Math.floor(size * 100) / 100.0} ${size_map[divided]}`;
+}
+
+const getDirSize = async (sDirectoryName) => {
+    const files = await glob.glob(`./${sDirectoryName}/**/*.*`);
+    return files.map(f => {
+        try {
+            return fs.statSync(f).size
+        } catch (_) {
+            return 0;
+        }
+    }).reduce((a, b) => { return a + b; }, 0);
+}
+(async () => {
+    const size = await getDirSize('dist');
+    console.log('Build size: ', byteSize(size));
+})();
