43385f6c8dba45693d51042741e17b0cfc52e90c
tabris87 <adrian_marten@hotmail.com>
2024-12-08 23:56:01 +0100

    chore(demo): added pageTemplate

--- temporary/interfacing/JourneyTemplate.ts
+++ temporary/interfacing/JourneyTemplate.ts
@@ -1,3 +1,6 @@
+const RootTemplate = require('./RootTemplate');
+const PageTemplate = require("./PageTemplate");
+
 const JSTemplate = "" +
     `/* global QUnit */
 sap.ui.define([
@@ -48,7 +51,7 @@ const TSMethodTemplate = "\n" +
     onThe{{page-name}}Page.{{method-name}}({{selector-object}});
     {{step-insert}}`;
 
-module.exports = class JourneyTemplate {
+module.exports = class JourneyTemplate extends RootTemplate {
 
     private _testName: string;
     private _appPrefix: string;
@@ -60,9 +63,11 @@ module.exports = class JourneyTemplate {
         function_name: string,
         step: Record<string, unknown>
     }[];
-    private _pages: Record<string, null> = {};
+    private _pages: Record<string, typeof PageTemplate> = {};
 
-    constructor() { }
+    constructor() {
+        super();
+    }
 
     extractAppPrefix(oJourneyJSON: Record<string, unknown>): JourneyTemplate {
         const aSteps = oJourneyJSON["steps"] as Record<string, unknown>[];
@@ -89,7 +94,14 @@ module.exports = class JourneyTemplate {
             const sPageName = oViewInfos.relativeViewName;
             let sStepSelector = JSON.stringify(oStep.recordReplaySelector, null, 2);
             sStepSelector = sStepSelector.replaceAll(/\n/gm, '\n\t\t');
-            this._pages[sPageName] = null;
+
+            if (!this._pages[sPageName]) {
+                const oViewInfos = oStep["viewInfos"] as { absoluteViewName: string, relativeViewName: string };
+                this._pages[sPageName] = new PageTemplate(oViewInfos.absoluteViewName);
+            }
+
+            this._pages[sPageName].addMethod(oStep.step);
+
             return {
                 step_comment: sComment,
                 step_type: sType,
@@ -108,6 +120,10 @@ module.exports = class JourneyTemplate {
         } else {
             return this._generateJSJourney();
         }
+    }
+
+    generatePages(bTypeScript: boolean = false): string[] {
+        return Object.values(this._pages).map(oP => oP.generate(bTypeScript));
     }
 
     private _generateJSJourney(): string {
@@ -162,47 +178,5 @@ module.exports = class JourneyTemplate {
             .replace("{{step-insert}}", "");
 
         return resultingJourney;
-    }
-
-    private _genMethodNameForStep(oStepJSON: Record<string, unknown>): string {
-
-        const sMethodName: string[] = [];
-        const aClassSpecifier = ((oStepJSON.control as Record<string, unknown>).type as string).split('.');
-
-        switch (oStepJSON.actionType) {
-            case 'input':
-                sMethodName.push('iType_');
-                sMethodName.push((oStepJSON.keys as Record<string, unknown>[]).reduce((agg: string, o: Record<string, unknown>) => agg + o.key, ''));
-                sMethodName.push('_IntoThe');
-                sMethodName.push(this._capitalizeFirstLetter(aClassSpecifier[aClassSpecifier.length - 1]));
-                break;
-            case 'clicked':
-                sMethodName.push('iPress');
-                sMethodName.push('The');
-                sMethodName.push(this._capitalizeFirstLetter(aClassSpecifier[aClassSpecifier.length - 1]));
-                break;
-            case 'validate':
-                sMethodName.push('iShouldSeeThe');
-                sMethodName.push(this._capitalizeFirstLetter(aClassSpecifier[aClassSpecifier.length - 1]));
-                break;
-            default:
-                sMethodName.push('xxx');
-        }
-        return sMethodName.join('');
-    }
-
-    private _capitalizeFirstLetter(sString: string): string {
-        const oNumberMap: Record<string, string> = {
-            '0': 'First',
-            '1': 'Second',
-            '2': 'Third',
-        };
-        if (isNaN(Number(sString))) {
-            return String(sString).charAt(0).toUpperCase() + String(sString).slice(1);
-        } else {
-            let sNumberWord = oNumberMap[sString];
-            sNumberWord = sNumberWord ? sNumberWord : (Number(sString) + 1) + 'th';
-            return sNumberWord;
-        }
     }
 }
---
+++ temporary/interfacing/RootTemplate.ts
@@ -0,0 +1,43 @@
+module.exports = class RootTemplate {
+    private _genMethodNameForStep(oStepJSON: Record<string, unknown>): string {
+
+        const sMethodName: string[] = [];
+        const aClassSpecifier = ((oStepJSON.control as Record<string, unknown>).type as string).split('.');
+
+        switch (oStepJSON.actionType) {
+            case 'input':
+                sMethodName.push('iType_');
+                sMethodName.push((oStepJSON.keys as Record<string, unknown>[]).reduce((agg: string, o: Record<string, unknown>) => agg + o.key, ''));
+                sMethodName.push('_IntoThe');
+                sMethodName.push(this._capitalizeFirstLetter(aClassSpecifier[aClassSpecifier.length - 1]));
+                break;
+            case 'clicked':
+                sMethodName.push('iPress');
+                sMethodName.push('The');
+                sMethodName.push(this._capitalizeFirstLetter(aClassSpecifier[aClassSpecifier.length - 1]));
+                break;
+            case 'validate':
+                sMethodName.push('iShouldSeeThe');
+                sMethodName.push(this._capitalizeFirstLetter(aClassSpecifier[aClassSpecifier.length - 1]));
+                break;
+            default:
+                sMethodName.push('xxx');
+        }
+        return sMethodName.join('');
+    }
+
+    private _capitalizeFirstLetter(sString: string): string {
+        const oNumberMap: Record<string, string> = {
+            '0': 'First',
+            '1': 'Second',
+            '2': 'Third',
+        };
+        if (isNaN(Number(sString))) {
+            return String(sString).charAt(0).toUpperCase() + String(sString).slice(1);
+        } else {
+            let sNumberWord = oNumberMap[sString];
+            sNumberWord = sNumberWord ? sNumberWord : (Number(sString) + 1) + 'th';
+            return sNumberWord;
+        }
+    }
+}
